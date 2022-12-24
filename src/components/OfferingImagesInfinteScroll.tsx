2619a7edba70cbc7320d2ee06205e9cf5daf54c2
Mithilesh <mithileshparmar1@gmail.com>
2022-12-24 19:01:27 +0530

    add: offering page images

--- src/components/OfferingImagesInfinteScroll.tsx
+++ src/components/OfferingImagesInfinteScroll.tsx
@@ -1,77 +1,47 @@
-import { createStyles, Image } from "@mantine/core";
 import "../App.css";
 
-const useStyles = createStyles((theme) => ({
-  wrapper: {
-    display: "flex",
-    alignItems: "center",
-    width: "100vw",
-    overflow: "hidden",
-  },
-
-  scrollParent: {
-    position: "relative",
-    width: "100vw",
-    height: "60vh",
-    overflowX: "hidden",
-  },
-
-  scrollElementPrimary: {
-    width: "inherit",
-    height: "inherit",
-    position: "absolute",
-    left: "0%",
-    top: "0%",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-evenly",
-    animation: "primary 10s linear infinite",
-  },
-
-  scrollElementSecondary: {
-    width: "inherit",
-    height: "inherit",
-    position: "absolute",
-    left: "0%",
-    top: "0%",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-evenly",
-    animation: "secondary 10s linear infinite",
-  },
-}));
 export function OfferingImagesInfinteScroll() {
-  const { classes, theme } = useStyles();
   return (
-    <div className={classes.wrapper}>
-      <div className={classes.scrollParent}>
-        <div className={classes.scrollElementPrimary}>
-          <Image
-            src={require("../assets/images/Desktop.png")}
-            width={"80%"}
-          ></Image>
-          <Image
-            src={require("../assets/images/Mobile.png")}
-            width={"55%"}
-          ></Image>
-          <Image
-            src={require("../assets/images/Desktop.png")}
-            width={"80%"}
-          ></Image>
+    <div className="slider">
+      <div className="slide-track-primary">
+        <div className="slide">
+          <img src={require("../assets/images/offering/Image1.jpeg")}></img>
+        </div>{" "}
+        <div className="slide">
+          <img src={require("../assets/images/offering/Image2.jpeg")}></img>
+        </div>{" "}
+        <div className="slide">
+          <img src={require("../assets/images/offering/Image3.jpeg")}></img>
+        </div>{" "}
+        <div className="slide">
+          <img src={require("../assets/images/offering/Image4.jpeg")}></img>
+        </div>{" "}
+        <div className="slide">
+          <img src={require("../assets/images/offering/Image5.jpeg")}></img>
         </div>
-        <div className={classes.scrollElementSecondary}>
-          <Image
-            src={require("../assets/images/Desktop.png")}
-            width={"80%"}
-          ></Image>
-          <Image
-            src={require("../assets/images/Mobile.png")}
-            width={"55%"}
-          ></Image>
-          <Image
-            src={require("../assets/images/Desktop.png")}
-            width={"80%"}
-          ></Image>
+        <div className="slide">
+          <img src={require("../assets/images/offering/Image6.jpeg")}></img>
+        </div>
+      </div>
+
+      <div className="slide-track-secondary">
+        <div className="slide">
+          <img src={require("../assets/images/offering/Image1.jpeg")}></img>
+        </div>
+        <div className="slide">
+          <img src={require("../assets/images/offering/Image2.jpeg")}></img>
+        </div>
+        <div className="slide">
+          <img src={require("../assets/images/offering/Image3.jpeg")}></img>
+        </div>
+        <div className="slide">
+          <img src={require("../assets/images/offering/Image4.jpeg")}></img>
+        </div>
+        <div className="slide">
+          <img src={require("../assets/images/offering/Image5.jpeg")}></img>
+        </div>
+        <div className="slide">
+          <img src={require("../assets/images/offering/Image6.jpeg")}></img>
         </div>
       </div>
     </div>
